fa75f5a1e3674fef1b9d9c97f107ba2cd5563c2b
Benjamin <benjy.chane@gmail.com>
2021-02-01 17:41:33 +0100

    slaiut'

--- src/java/main.cs
+++ src/java/main.cs
@@ -1,8 +1,21 @@
 public class Account {
-	public void saveCustomReportAccessAttempt(String value) {
+  @get
+  public String getCurrentLoadedReport() {
+    // custom implementation of DB read access - input
+    String internal_report_id = external_exec.db.read.custom.session.getLoadedReport();
+    return internal_report_id;
+  }
+ 
+  @post
+  public void saveCustomReportError(String value) {
+    // custom implementation of DB write access - output
+    external_exec.db.write.custom.reportError("UPDATE tb.log.error SET msg=344 WHERE id=" + value);
+  }
+ 
+  @post
+  public void saveCustomReportAccessAttempt(String value) {
     value = make_safe(value);
     // custom implementation of DB write access - output
     external_exec.db.write.custom.accessAttempt("UPDATE tb.log.error SET msg=406 WHERE id=" + value);
   }
-}
 }
